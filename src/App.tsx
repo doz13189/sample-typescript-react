f4b0757e8a0bff06a7280bc9ca90e45cb704518a
takahiro kato <dozo13189@gmail.com>
2022-03-09 00:50:41 +0900

    9-47

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Practice1 } from './9/Practice/Practice1';
 
 function App() {
   return (
     <div className="App">
+      <Practice1 />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
